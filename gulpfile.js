1978f482453aa2ae86e09cc214cfb8c9e8e5d14c
baiyaaaaa <bigpet1991@gmail.com>
2016-05-09 01:47:01 +0800

    hehe

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
-var postcss = require('postcss');
+var postcss = require('gulp-postcss');
+var fs = require('fs');
+var path = require('path');
 
 gulp.task('default', () => {
   return gulp.src('./src/**/*.css').pipe(
     postcss([
-      require('postcss-salad')({ /* options */ })
+      require('postcss-salad')({ /* options */ }),
+      require('postcss-discard-comments')({})
     ])
-  ).pipe(
-    gulp.dest('./asserts')
-  );
+  ).pipe(gulp.dest('./asserts'));
 });
 
 gulp.watch('./src/**/*.css', ['default']);
