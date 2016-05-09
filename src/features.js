209dee7f349795d1330243632b2b171744a8e4ed
baiyaaaaa <bigpet1991@gmail.com>
2016-05-09 14:36:55 +0800

    update feature sequence

--- src/features.js
+++ src/features.js
@@ -5,21 +5,18 @@ export default {
    * order is important
    * ******************
    */
-
+  
+  // https://npmjs.com/package/precss
+  precss: (options) => require("precss")(options),
+  
+  // https://npmjs.com/package/postcss-css-reset
+  reset: (options) => require("postcss-css-reset")(options),
+  
   // https://npmjs.com/package/postcss-utils
   utils: (options) => require("postcss-utils")(options),
 
-  // https://npmjs.com/package/postcss-css-reset
-  reset: (options) => require("postcss-css-reset")(options),
-
-  // https://npmjs.com/package/postcss-neat
-  neat: (options) => require("postcss-neat")(options),
-
   // https://npmjs.com/package/postcss-bem
   bem: (options) => require("postcss-bem")(options),
-
-  // https://npmjs.com/package/precss
-  precss: (options) => require("precss")(options),
 
   // https://npmjs.com/package/postcss-calc
   calc: (options) => require("postcss-calc")(options),
@@ -40,5 +37,8 @@ export default {
   rem: (options) => require("pixrem")(options),
 
   // https://npmjs.com/package/autoprefixer
-  autoprefixer: (options) => require("autoprefixer")(options)
+  autoprefixer: (options) => require("autoprefixer")(options),
+  
+  // https://npmjs.com/package/postcss-neat
+  neat: (options) => require("postcss-neat")(options)
 }
