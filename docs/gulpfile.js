ee4924914253b0326e4202ba762d2f34ade2b232
baiyaaaaa <bigpet1991@gmail.com>
2016-12-07 13:41:35 +0800

    add docs to master branch

---
+++ docs/gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var fs = require('fs');
+var path = require('path');
+
+gulp.task('default', () => {
+  return gulp.src('./_css/*.css').pipe(
+    postcss([
+      require('postcss-salad')({ /* options */ }),
+      require('postcss-discard-comments')({})
+    ])
+  ).pipe(gulp.dest('./css'));
+});
+
+gulp.watch('./_css/*.css', ['default']);
